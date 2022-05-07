2d3ff7fc734eeda81d543e76d411756d2cbdce17
MinseoPark <kjms0820@gmail.com>
2022-05-07 19:12:39 +0900

    Design Main Page

--- app/src/main/java/com/example/intothe/Test/TestListView.java
+++ app/src/main/java/com/example/intothe/Test/TestListView.java
@@ -3,6 +3,7 @@ package com.example.intothe.Test;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -11,18 +12,31 @@ import com.example.intothe.MainActivity;
 import com.example.intothe.R;
 import com.google.android.material.tabs.TabLayout;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class TestListView extends AppCompatActivity {
 
     TestList testList;
+    long mNow;
+    Date mDate;
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_list_view);
 
+        TextView testDate = (TextView) findViewById(R.id.testDate);
+
         testList = new TestList();
+
+        // 테스트 날짜 가져오기
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+        testDate.setText("시행날짜 " + mFormat.format(mDate));
+
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, testList).commit();
